d689565acc026bd4393c67cd548aca282bcf33b6
Brandon Yuong <brandonyuong@gmail.com>
2019-11-01 22:08:39 -0700

    Finished implementing all plots for analysis.

--- unsupervised_learning/PCAPlots.py
+++ unsupervised_learning/PCAPlots.py
@@ -2,14 +2,52 @@ import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.decomposition import PCA
 from unsupervised_learning.KMeansPlots import KMeansPlots
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
 
 
 class PCAPlots(object):
-    def __init__(self, x_train, data_name, n=4):
+    def __init__(self, x_train, y_train, x_test, y_test, data_name, n=4):
         self.x_train = x_train
+        self.y_train = y_train
+        self.x_test = x_test
+        self.y_test = y_test
         self.data_name = data_name
         self.n = n
         self.plot_explained_var_ratio()
+        self.plot_rf_accuracy()
+
+    def plot_rf_accuracy(self):
+        n = self.n
+        acc = np.zeros(n)
+        range_n_components = np.arange(1, n + 1)
+        for i, j in enumerate(range_n_components):
+            dim_reducer = PCA(n_components=j)
+            transformed_x = dim_reducer.fit_transform(self.x_train, self.y_train)
+            model = RandomForestClassifier()
+            model.fit(transformed_x, self.y_train)
+            test = dim_reducer.transform(self.x_test)
+            rf_acc = accuracy_score(model.predict(test), self.y_test)
+            acc[i] = rf_acc
+
+        model = RandomForestClassifier(n_estimators=10)
+        model.fit(self.x_train, self.y_train)
+        baseline = accuracy_score(model.predict(self.x_test), self.y_test)
+
+        plt.figure()
+        title = self.data_name + " PCA Random Forest Performance"
+        plt.title(title)
+        plt.xlabel("n-components")
+        plt.ylabel("Accuracy")
+        plt.grid()
+
+        plt.plot(range_n_components, acc, 'o-', color="#f92672",
+                 label='PCA')
+        plt.plot(range_n_components, [baseline] * len(acc), color="#007fff",
+                 label='Baseline Model')
+
+        plt.savefig("plots/" + title + ".png")
+        plt.close()
 
     def plot_explained_var_ratio(self):
         X = self.x_train
--- unsupervised_learning/main.py
+++ unsupervised_learning/main.py
@@ -10,8 +10,6 @@ from unsupervised_learning.ICAPlots import ICAPlots
 from unsupervised_learning.RPPlots import RPPlots
 from unsupervised_learning.LDAPlots import LDAPlots
 from unsupervised_learning.NNPlots import NNPlots
-from sklearn.cluster import KMeans
-from sklearn.metrics import mutual_info_score
 
 
 def main():
@@ -22,22 +20,22 @@ def main():
 
     scaled_x1 = scale_features(x1)
     scaled_x2 = scale_features(x2)
-    """
+
     plot_learning_curve(MLPClassifier(), "Seeds Data NN", scaled_x1, y1)
     plot_fit_times(MLPClassifier(), "Seeds Data NN Fit Time", scaled_x1, y1)
     plot_learning_curve(MLPClassifier(), "Driver Data NN", scaled_x2, y2)
     plot_fit_times(MLPClassifier(), "Driver Data NN Fit Time", scaled_x2, y2)
-    """
+
     print("*** Seeds Data ***")
     x1_train, x1_test, y1_train, y1_test = ms.train_test_split(
         scaled_x1, y1, train_size=120, random_state=2)
-    #MLPAnalysis(x1_train, x1_test, y1_train, y1_test, random_state=2)
+    MLPAnalysis(x1_train, x1_test, y1_train, y1_test, random_state=2)
 
     print("*** Driver Face Data ***")
     x2_train, x2_test, y2_train, y2_test = ms.train_test_split(
-        scaled_x2, y2, train_size=400, random_state=2)
-    #MLPAnalysis(x2_train, x2_test, y2_train, y2_test, random_state=2)
-    """
+        scaled_x2, y2, train_size=350, random_state=2)
+    MLPAnalysis(x2_train, x2_test, y2_train, y2_test, random_state=2)
+
     print("*** KMeans ***")
     seeds_km = KMeansPlots(x1_train, y1_train, "Seeds", 6)
     driver_km = KMeansPlots(x2_train, y2_train, "Driver", 16)
@@ -47,52 +45,51 @@ def main():
     print("*** EM ***")
     EMPlots(x1_train, y1_train, "Seeds", 6)
     EMPlots(x2_train, y2_train, "Driver", 16)
-"""
+    
     print("*** PCA ***")
-    seeds_pca = PCAPlots(x1_train, "Seeds", 6)
-    driver_pca = PCAPlots(x2_train, "Driver", 16)
-    transformed = seeds_pca.get_transformed_features(3)
+    seeds_pca = PCAPlots(x1_train, y1_train, x1_test, y1_test, "Seeds", 6)
+    driver_pca = PCAPlots(x2_train, y2_train, x2_test, y2_test, "Driver", 16)
+    transformed = seeds_pca.get_transformed_features(2)
     KMeansPlots(transformed, y1_train, "Seeds with PCA", 6)
     EMPlots(transformed, y1_train, "Seeds with PCA", 6)
-    transformed = driver_pca.get_transformed_features(6)
-    KMeansPlots(transformed, y2_train, "Driver with PCA", 9)
-    EMPlots(transformed, y2_train, "Driver with PCA", 9)
-    """
+    transformed = driver_pca.get_transformed_features(7)
+    KMeansPlots(transformed, y2_train, "Driver with PCA", 16)
+    EMPlots(transformed, y2_train, "Driver with PCA", 16)
+
     print("*** ICA ***")
-    seeds_ica = ICAPlots(x1_train, "Seeds", 6)
-    driver_ica = ICAPlots(x2_train, "Driver", 16)
+    seeds_ica = ICAPlots(x1_train, y1_train, x1_test, y1_test, "Seeds", 6)
+    driver_ica = ICAPlots(x2_train, y2_train, x2_test, y2_test, "Driver", 16)
     transformed = seeds_ica.get_transformed_features(6)
-    KMeansPlots(transformed, y1_train, "Seeds with ICA", 9)
-    EMPlots(transformed, y1_train, "Seeds with ICA", 9)
+    KMeansPlots(transformed, y1_train, "Seeds with ICA", 6)
+    EMPlots(transformed, y1_train, "Seeds with ICA", 6)
     transformed = driver_ica.get_transformed_features(10)
-    KMeansPlots(transformed, y2_train, "Driver with ICA", 13)
-    EMPlots(transformed, y2_train, "Driver with ICA", 13)
-    """
+    KMeansPlots(transformed, y2_train, "Driver with ICA", 16)
+    EMPlots(transformed, y2_train, "Driver with ICA", 16)
+    
     print("*** RP ***")
     seeds_rp = RPPlots(x1_train, y1_train, x1_test, y1_test, "Seeds", 6)
     driver_rp = RPPlots(x2_train, y2_train, x2_test, y2_test, "Driver", 16)
-    transformed = seeds_rp.get_transformed_features(4)
-    KMeansPlots(transformed, y1_train, "Seeds with RP", 7)
-    EMPlots(transformed, y1_train, "Seeds with RP", 7)
-    transformed = driver_rp.get_transformed_features(6)
-    KMeansPlots(transformed, y2_train, "Driver with RP", 9)
-    EMPlots(transformed, y2_train, "Driver with RP", 9)
-    """
+    transformed = seeds_rp.get_transformed_features(6)
+    KMeansPlots(transformed, y1_train, "Seeds with RP", 6)
+    EMPlots(transformed, y1_train, "Seeds with RP", 6)
+    transformed = driver_rp.get_transformed_features(16)
+    KMeansPlots(transformed, y2_train, "Driver with RP", 16)
+    EMPlots(transformed, y2_train, "Driver with RP", 16)
+    
     print("*** LDA ***")
     seeds_lda = LDAPlots(x1_train, y1_train, x1_test, y1_test, "Seeds", 6)
     driver_lda = LDAPlots(x2_train, y2_train, x2_test, y2_test, "Driver", 16)
-    
     transformed = seeds_lda.get_transformed_features(2)
-    KMeansPlots(transformed, y1_train, "Seeds with LDA", 5)
-    EMPlots(transformed, y1_train, "Seeds with LDA", 5)
+    KMeansPlots(transformed, y1_train, "Seeds with LDA", 6)
+    EMPlots(transformed, y1_train, "Seeds with LDA", 6)
     transformed = driver_lda.get_transformed_features(3)
-    KMeansPlots(transformed, y2_train, "Driver with LDA", 6)
-    EMPlots(transformed, y2_train, "Driver with LDA", 6)
+    KMeansPlots(transformed, y2_train, "Driver with LDA", 16)
+    EMPlots(transformed, y2_train, "Driver with LDA", 16)
 
     print("*** NN ***")
-    NNPlots(x1_train, y1_train, x1_test, y1_test, "Seeds", [3, 6, 4, 2], [4, 3])
-    NNPlots(x2_train, y2_train, x2_test, y2_test, "Driver", [6, 10, 6, 3], [4, 8])
-"""
+    NNPlots(x1_train, y1_train, x1_test, y1_test, "Seeds", [2, 3, 6, 2], [3, 3])
+    NNPlots(x2_train, y2_train, x2_test, y2_test, "Driver", [7, 11, 16, 3], [5, 5])
+
 
 if __name__ == '__main__':
     main()
